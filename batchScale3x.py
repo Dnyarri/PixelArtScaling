7023b3ca9acba4f9c05c62ba3e25ce2fd29890b4
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-04-27 06:48:09 +0300

    temp reincarnation of blocking UI

--- batchScale3x.py
+++ batchScale3x.py
@@ -10,12 +10,6 @@ Versions:
 2024.02.24  Cleanup, GUI tweaks, versioning changed to YYYY.MM.DD
 2024.03.30  pHYs chunk editing to keep image print size constant
 2024.04.03  pathlib Path.exists flightcheck to make GUI Nuitka-proof
-2024.04.23  Self-calling scalefile(runningfilename)
-2024.04.23  Multiprocessing!!!
-            pool.map version
-            Unfortunately, GUI went to hell (mostly)
-2024.04.24  Async variants halve processing time, pool.map_async will go to production
-2024.04.26  GUI still sucks and not updated, but now it apologize
 
 '''
 
@@ -23,29 +17,72 @@ __author__ = "Ilya Razmanov"
 __copyright__ = "(c) 2024 Ilya Razmanov"
 __credits__ = "Ilya Razmanov"
 __license__ = "unlicense"
-__version__ = "2024.04.26"
+__version__ = "2024.04.03"
 __maintainer__ = "Ilya Razmanov"
 __email__ = "ilyarazmanov@gmail.com"
 __status__ = "Production"
 
-from tkinter import Tk, Label, filedialog, X
-from pathlib import Path
-from glob import glob
-from multiprocessing import Pool
+from tkinter import Tk, Label, Text, filedialog, ttk, X, BOTH
 
-import png                      # PNG reading: PyPNG from:  https://gitlab.com/drj11/pypng
-import IncSrc                   # Image reshaping from:     https://github.com/Dnyarri/PixelArtScaling
+from pathlib import Path
+
+from glob import glob
+
+import png                      # PNG reading: PyPNG from: https://gitlab.com/drj11/pypng
+import IncSrc                   # Image reshaping from: https://github.com/Dnyarri/PixelArtScaling
 from IncScaleNx import Scale3x  # Scale2x and Scale3x from: https://github.com/Dnyarri/PixelArtScaling
 
+# --------------------------------------------------------------
+# Creating dialog
 
-def scalefile(runningfilename):
-    '''
-    Function that does all the job, and keeps quite. 
+iconpath = Path(__file__).resolve().parent / '3xBATCH.ico'
+iconname = str(iconpath)
+useicon = iconpath.exists() # Check if icon file really exist. If False, it will not be used later.
 
-    '''
+sortir = Tk()
+sortir.title('Processing Scale3x...')
+if useicon:
+    sortir.iconbitmap(iconname)
+sortir.geometry('+100+100')
+zanyato = Label(sortir, text='Starting...', font=("arial", 12), padx=16, pady=10, justify='center')
+zanyato.pack()
+progressbar =  ttk.Progressbar(orient="horizontal", mode="indeterminate")
+progressbar.pack(fill=X, expand=True)
+pogovorit = Text(sortir, height=26, wrap='word', state='normal')
+pogovorit.pack(fill=BOTH, expand=True)
+pogovorit.insert('1.0', 'Allons-y!\n')
+sortir.withdraw()
+
+# Main dialog created and hidden
+# --------------------------------------------------------------
+
+# Open source dir
+sourcedir = filedialog.askdirectory(title='Open DIR to resize PNG images using Scale3x')
+if sourcedir == '':
+    quit()
+
+# --------------------------------------------------------------
+# Updating dialog
+
+sortir.deiconify()
+zanyato.config(text='Allons-y!')
+sortir.update()
+sortir.update_idletasks()
+
+# Dialog shown and updated
+# --------------------------------------------------------------
+
+# Process file list
+for runningfilename in glob(sourcedir + "/**/*.png", recursive=True):  # select all PNG files in all subfolders
 
     oldfile = runningfilename
     newfile = oldfile  # Previous version used backup newfile = oldfile + '.3x.png'
+
+    zanyato.config(text=f'Currently processing {oldfile}')  # Updating label, showing processed file name
+    progressbar.start(50)
+    pogovorit.insert('end -1 chars', f' Starting {oldfile}... ')
+    sortir.update()
+    sortir.update_idletasks()
 
     source = png.Reader(filename=oldfile)
     X, Y, pixels, info = source.asDirect()  # Opening image, iDAT comes to "pixels" as bytearray, to be tuple'd lated.
@@ -55,11 +92,17 @@ def scalefile(runningfilename):
     # Reading image as list
     ImageAsListListList = IncSrc.Img3D(imagedata, X, Y, Z)
 
+    progressbar.start(50)
+
     # Scaling list to 3x image list
     EPXImage, newX, newY = Scale3x(ImageAsListListList, X, Y)
 
+    progressbar.start(50)
+
     # Reshaping 3x scaled 3D list into 1D list for PyPNG .write_array method
     ResultImageAsList = IncSrc.Img3Dto1D(EPXImage, newX, newY, Z)
+
+    progressbar.start(50)
 
     # --------------------------------------------------------------
     # Fixing resolution to match original print size.
@@ -78,6 +121,8 @@ def scalefile(runningfilename):
     # Resolution changed
     # --------------------------------------------------------------
 
+    progressbar.start(50)
+
     # Writing new image
     resultPNG = open(newfile, mode='wb')
     writer = png.Writer(
@@ -90,48 +135,14 @@ def scalefile(runningfilename):
     )
     writer.write_array(resultPNG, ResultImageAsList)
     resultPNG.close()
-# end scalefile, no return
 
-if __name__ == '__main__':
+    progressbar.start(50)
+    pogovorit.insert('end -1 chars', ' Done\n')
+# --------------------------------------------------------------
+# Destroying dialog
 
-    # Creating dialog
-    iconpath = Path(__file__).resolve().parent / '3xBATCH.ico'
-    iconname = str(iconpath)
-    useicon = iconpath.exists() # Check if icon file really exist. If False, it will not be used later.
+sortir.destroy()
+sortir.mainloop()
 
-    sortir = Tk()
-    sortir.title('Processing Scale3x...')
-    if useicon:
-        sortir.iconbitmap(iconname)
-    sortir.geometry('+100+100')
-    zanyato = Label(sortir, text='Allons-y!', font=("Arial", 16), state='normal', padx=12, pady=10, justify='center')
-    zanyato.pack()
-    small = Label(sortir, text='At 100% CPU load GUI tend to become unresponsive.\nWe apologize for making image processing as fast as possible.', font=("Courier", 10), state='disabled', padx=12, pady=10, justify='center')
-    small.pack(fill=X)
-    sortir.withdraw()
-    # Main dialog created and hidden
-    
-    # Open source dir
-    sourcedir = filedialog.askdirectory(title='Open DIR to resize PNG images using Scale3x')
-    if sourcedir == '' or sourcedir == None:
-        quit()
-
-    # Updating dialog
-    sortir.deiconify()
-    zanyato.config(text='Asynchronous processes ensued, pronto!')
-    sortir.update()
-    sortir.update_idletasks()
-    
-    # Creating pool
-    scalepool = Pool()
-
-    # Feeding pool (no pun!)
-    scalepool.map_async(scalefile,glob(sourcedir + "/**/*.png", recursive=True))
-
-    # Everything fed into the pool, waiting and closing
-    scalepool.close()
-    scalepool.join()
-
-    # Destroying dialog
-    sortir.destroy()
-    sortir.mainloop()
+# Dialog destroyed and closed
+# --------------------------------------------------------------
