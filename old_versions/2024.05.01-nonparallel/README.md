e86f601074384db63e6addd2492f96de6fa851c3
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-05-01 10:02:24 +0300

    last non-parallel version in old_versions

---
+++ old_versions/2024.05.01-nonparallel/README.md
@@ -0,0 +1,42 @@
+**(EN)** [(RU)](README.RU.md)
+
+# Pixel Art Scaling programs
+
+Programs for scaling up small low-color images like icons and game sprites.
+
+Apparently useful for scaling up grey text scans with low resolution before OCR, to improve OCR quality.
+
+> [!NOTE]
+> Only PNG image format is supported. Batch processing of folders and subfolders is supported.
+
+Currently **Scale2x** (aka **AdvMAME2x**) and **Scale3x** (aka **AdvMAME3x**) are implemented.
+
+- Scale2xGUI.py - uses Scale2x scaling, equipped with simple GUI for opening and saving PNG files.
+- Scale2xCLI.py - same Scale2x scaling as above, runs with command line:  
+        *python Scale2xCLI.py input.png output.png*
+- batchScale2x.py - batch rescaling of all PNG files within chosen directory, recursively, using Scale2x scaling. Source images are replaced, no backup - no mercy.  
+
+- Scale3xGUI.py - uses Scale3x scaling, equipped with simple GUI for opening and saving PNG files.
+- Scale3xCLI.py - same Scale3x scaling as above, runs with command line:  
+        *python Scale3xCLI.py input.png output.png*
+- batchScale3x.py - batch rescaling of all PNG files within chosen directory, recursively, using Scale3x scaling. Source images are replaced, no backup - no mercy.  
+
+## Sample of Scale3x output (twice)
+
+![Example of Scale3x run twice](https://dnyarri.github.io/imgscalenx/x3x3.png)
+
+*Dependencies:* [PyPNG](https://gitlab.com/drj11/pypng), Tkinter, sys
+
+> [!NOTE]
+> Programs are written entirely on Python, using image representation as list of lists of lists.
+> While this representation is logical for human understanding, Python processing of this is slow.
+> Moreover, programs contain lists reshaping for PNG I/O purposes, also performed using Python native means only.
+> As a result, programs are slow but quite compatible with anything and don't require large external packages.
+
+Related links:
+
+[Dnyarri website](https://dnyarri.github.io)
+
+[github Dnyarri](https://github.com/Dnyarri)
+
+[gitflic Dnyarri](https://gitflic.ru/user/dnyarri)
---
+++ old_versions/2024.05.01-nonparallel/Scale2xCLI.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+'''
+Scale2x aka AdvMAME2x bitmap image scaling using Python only, command line version
+Created by: Ilya Razmanov (mailto:ilyarazmanov@gmail.com)
+            aka Ilyich the Toad (mailto:amphisoft@gmail.com)
+
+Usage: python Scale2xCLI.py source.png result.png
+
+Versions:
+01.001    Initial working Scale2x (AdvMAME2x) release 
+01.002    Changed from self-contained to modular, IncSrc and IncScaleNx modules created 
+01.003    Ultimate modular evil, moving everything possible to IncSrc.py and IncScaleNx.py 
+2024.02.24  Cleanup, minimizing import, versioning changed to YYYY.MM.DD
+2024.03.30  pHYs chunk editing to keep image print size constant
+
+'''
+
+__author__ = "Ilya Razmanov"
+__copyright__ = "(c) 2024 Ilya Razmanov"
+__credits__ = "Ilya Razmanov"
+__license__ = "unlicense"
+__version__ = "2024.03.30"
+__maintainer__ = "Ilya Razmanov"
+__email__ = "ilyarazmanov@gmail.com"
+__status__ = "Production"
+
+from sys import argv
+
+import png                      # PNG reading: PyPNG from: https://gitlab.com/drj11/pypng
+import IncSrc                   # Image reshaping from: https://github.com/Dnyarri/PixelArtScaling
+from IncScaleNx import Scale2x  # Scale2x and Scale3x from: https://github.com/Dnyarri/PixelArtScaling
+
+# Taking user input
+
+Rez = argv[1]
+Dvo = argv[2]
+
+# Open source file
+source = png.Reader(filename=Rez)
+
+X, Y, pixels, info = source.asDirect()  # Opening image, iDAT comes to "pixels" as bytearray, to be tuple'd lated.
+Z = info['planes']                      # Maximum CHANNEL NUMBER
+imagedata = tuple((pixels))             # Attempt to fix all bytearrays
+
+# Source file opened as imagedata
+# --------------------------------------------------------------
+
+
+# Reading image as list
+ImageAsListListList = IncSrc.Img3D(imagedata, X, Y, Z)
+
+# Scaling list to 2x image list
+EPXImage, doubleX, doubleY = Scale2x(ImageAsListListList, X, Y)
+
+# Reshaping 2x scaled 3D list into 1D list for PyPNG .write_array method
+ResultImageAsList = IncSrc.Img3Dto1D(EPXImage, doubleX, doubleY, Z)
+
+# --------------------------------------------------------------
+# Fixing resolution to match original print size.
+# If no pHYs found in original, 96 ppi is assumed as original value.
+if 'physical' in info:
+    res = info['physical']      # Reading resolution as tuple
+    x_pixels_per_unit = res[0]
+    y_pixels_per_unit = res[1]
+    unit_is_meter = res[2]
+else:
+    x_pixels_per_unit = 3780    # 3780 px/meter = 96 px/inch, 2834 px/meter = 72 px/inch
+    y_pixels_per_unit = 3780    # 3780 px/meter = 96 px/inch, 2834 px/meter = 72 px/inch
+    unit_is_meter = True
+x_pixels_per_unit = 2 * x_pixels_per_unit   # Double resolution to keep print size
+y_pixels_per_unit = 2 * y_pixels_per_unit   # Double resolution to keep print size
+# Resolution changed
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Open export file
+
+resultPNG = open(Dvo, mode='wb')
+# Writing export file
+writer = png.Writer(
+    doubleX,
+    doubleY,
+    greyscale=info['greyscale'],
+    alpha=info['alpha'],
+    bitdepth=info['bitdepth'],
+    physical=[x_pixels_per_unit, y_pixels_per_unit, unit_is_meter],
+)
+writer.write_array(resultPNG, ResultImageAsList)
+resultPNG.close()
+
+# Export file closed
+# --------------------------------------------------------------
